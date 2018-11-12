e0cf5c32b2ad39ca20b18cae5d88ea214ea16ae7
Sylvain Peigney <sylvain@volpeo.fr>
2018-11-12 10:10:55 +0100

    add flats management

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,72 @@
+class FlatsController < ApplicationController
+
+  # GET /flats
+  # GET /flats.json
+  def index
+    @flats = Flat.all
+  end
+
+  # GET /flats/1
+  # GET /flats/1.json
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  # GET /flats/new
+  def new
+    @flat = Flat.new
+  end
+
+  # GET /flats/1/edit
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  # POST /flats
+  # POST /flats.json
+  def create
+    @flat = Flat.new(flat_params)
+
+    respond_to do |format|
+      if @flat.save
+        format.html { redirect_to @flat, notice: 'Flat was successfully created.' }
+        format.json { render :show, status: :created, location: @flat }
+      else
+        format.html { render :new }
+        format.json { render json: @flat.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /flats/1
+  # PATCH/PUT /flats/1.json
+  def update
+    @flat = Flat.find(params[:id])
+    respond_to do |format|
+      if @flat.update(flat_params)
+        format.html { redirect_to @flat, notice: 'Flat was successfully updated.' }
+        format.json { render :show, status: :ok, location: @flat }
+      else
+        format.html { render :edit }
+        format.json { render json: @flat.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /flats/1
+  # DELETE /flats/1.json
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    respond_to do |format|
+      format.html { redirect_to flats_url, notice: 'Flat was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def flat_params
+      params.require(:flat).permit(:title, :address)
+    end
+end
---
+++ app/views/flats/_flat.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! flat, :id, :title, :address, :created_at, :updated_at
+json.url flat_url(flat, format: :json)
