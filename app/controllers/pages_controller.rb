810d7332c8a78aacfb586d314d7cb21482c79118
Sylvain Peigney <sylvain@volpeo.fr>
2018-11-12 09:57:51 +0100

    a lot of stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home]
+
   def home
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+
+    @pet = current_user.pets.new(pet_params)
+
   end
 end
